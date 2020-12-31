5823e50dc6f583d0927d0fd16e421c70943201ed
tensorway <darijan.gudelj@gmail.com>
2020-12-31 12:44:00 +0100

    added saving to visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,13 +1,23 @@
 import gym
 import numpy as np
 import time
+import cv2
 
-def showit(env, act, time_delta=0.005, max_steps=2000):
+def showit(env, act, time_delta=0.005, max_steps=2000, save_file=None, fps=50):
     dons = 0
     observation = env.reset()
     total_rew = 0
+    if save_file is not None:
+        img = env.render(mode="rgb_array")
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        out = cv2.VideoWriter(save_file ,fourcc, fps, (img.shape[1], img.shape[0]))
     for t in range(max_steps):
-            env.render()
+            if save_file is None:
+                env.render()
+            else:
+                img = env.render(mode="rgb_array")
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                out.write(img)
             obser = np.expand_dims(observation, axis=0)
             action, _= act(obser)
             #print(action, _)
@@ -22,6 +32,7 @@ def showit(env, act, time_delta=0.005, max_steps=2000):
                 break
             time.sleep(time_delta)
             print(t, "/"+str(max_steps), end='\r')
+    if save_file is not None:
+        out.release()
     env.close()
     return total_rew
-    
